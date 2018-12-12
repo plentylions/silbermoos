3510db0736052f955d42de779e414166388adc60
Misha Yatsenko <1234asdfm@gmail.com>
2018-12-12 15:28:51 +0200

    init

---
+++ src/Providers/SilbermoosServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Silbermoos\Providers;
+
+use IO\Helper\TemplateContainer;
+use IO\Helper\ResourceContainer;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
+
+
+/**
+ * Class SilbermoosServiceProvider
+ * @package Silbermoos\Providers
+ */
+class SilbermoosServiceProvider extends ServiceProvider
+{
+    const PRIORITY = 0;
+
+    public function register()
+    {
+
+    }
+
+    public function boot(Twig $twig, Dispatcher $dispatcher)
+    {
+        $dispatcher->listen('IO.Resources.Import', function (ResourceContainer $container) {
+            $container->addStyleTemplate('Silbermoos::Stylesheet');
+        }, self::PRIORITY);
+    }
+}
+
