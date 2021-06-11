877c44855777b6b37d6d22f92b5fcdb3405f3ed8
plentylionsgit <info@plentylions.de>
2021-06-11 10:05:14 +0300

    dev

--- src/Providers/SilbermoosServiceProvider.php
+++ src/Providers/SilbermoosServiceProvider.php
@@ -4,6 +4,7 @@ namespace Silbermoos\Providers;
 
 use IO\Helper\TemplateContainer;
 use IO\Helper\ResourceContainer;
+use IO\Helper\ComponentContainer;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
@@ -26,6 +27,14 @@ class SilbermoosServiceProvider extends ServiceProvider
         foreach ($widgetClasses as $widgetClass) {
             $widgetRepository->registerWidget($widgetClass);
         }
+
+        $dispatcher->listen('IO.Component.Import', function (ComponentContainer $componentContainer)
+        {
+            if($componentContainer->getOriginComponentTemplate() == 'Legend::WishList.Components.WishListCount'){
+                $componentContainer->setNewComponentTemplate('Silbermoos::WishList.Components.WishListCount');
+            }
+        }, self::PRIORITY);
+
 
         $dispatcher->listen('IO.Resources.Import', function (ResourceContainer $container) {
             $container->addStyleTemplate('Silbermoos::Stylesheet');
